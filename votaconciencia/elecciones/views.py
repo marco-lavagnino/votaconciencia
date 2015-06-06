b7e67dfa44efb567e6699526aefd5909809ed461
Marco Lavagnino <marco.lavagnino@gmail.com>
2015-06-06 17:37:59 -0300

    subido con power

--- votaconciencia/elecciones/views.py
+++ votaconciencia/elecciones/views.py
@@ -10,3 +10,19 @@ def index (request):
 
 def index_elecciones(request):
     return render(request, 'home/elecciones_index.html', {'elecciones': Eleccion.objects.all()})
+
+
+def eleccion_individual(request, id):
+    tuplas = []
+
+    for partido in Partido.objects.all():
+        p = partido
+        candidatos = Candidato.objects.filter(eleccion__id=id)
+        tuplas.append((p, candidatos))
+
+    dictionary = {
+        'partido_candidatos' : tuplas,
+        'eleccion': Eleccion.objects.get(id=id)
+    }
+
+    return render(request, 'home/eleccion_individual.html', dictionary)
