3845b8e8ba662e08fd93d09981ee3ecb8785de0e
julian <julian-allende@hotmail.com>
2015-06-06 12:48:39 -0300

    primer index

--- votaconciencia/elecciones/views.py
+++ votaconciencia/elecciones/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
 
-# Create your views here.
+def index (request):
+    return render_to_response('home/index.html',ctx,
+    context_instance=RequestContext(request)
+    )
