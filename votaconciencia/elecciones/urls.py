12b4641e5a8b06f460d26a513727a26e3fdb075e
Marco Lavagnino <marco.lavagnino@gmail.com>
2015-06-06 17:51:35 -0300

    ayuda

--- votaconciencia/elecciones/urls.py
+++ votaconciencia/elecciones/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     url(r'perfil/(?P<idc>\d+)', views.index_perfil , name='perfil'),
     url(r'candidatos', views.index_candidatos , name='candidatos'),
     url(r'elecciones', views.index_elecciones , name='elecciones'),
+    url(r'eleccion/(?P<id>\d+)', views.eleccion_individual, name='eleccion_individual'),
     url(r'$^', views.index , name='inicio'),
 )
 
