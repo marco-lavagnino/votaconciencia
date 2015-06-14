71d3835dd01a8ee621082632e1ac471f2ea95f5d
Marco Lavagnino <marco.lavagnino@gmail.com>
2015-06-14 20:31:14 +0000

    migracion para app de encuestas

--- votaconciencia/entrevistas/migrations/0001_initial.py
+++ votaconciencia/entrevistas/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0001_initial'),
+        ('elecciones', '__first__'),
     ]
 
     operations = [
@@ -15,13 +15,55 @@ class Migration(migrations.Migration):
             name='Entrevista',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('object_id', models.PositiveIntegerField()),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('contenido', models.TextField()),
             ],
             options={
                 'verbose_name': 'Entrevista',
                 'verbose_name_plural': 'Entrevistas',
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='EntrevistaCandidato',
+            fields=[
+                ('entrevista_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='entrevistas.Entrevista')),
+                ('entrevistado', models.ForeignKey(to='elecciones.Candidato')),
+            ],
+            options={
+                'verbose_name': 'Entrevista a candidato',
+                'verbose_name_plural': 'Entrevistas a candidatos',
+            },
+            bases=('entrevistas.entrevista',),
+        ),
+        migrations.CreateModel(
+            name='EntrevistaPersonalidad',
+            fields=[
+                ('entrevista_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='entrevistas.Entrevista')),
+            ],
+            options={
+                'verbose_name': 'Entrevista a personalidad',
+                'verbose_name_plural': 'Entrevistas a personalidades',
+            },
+            bases=('entrevistas.entrevista',),
+        ),
+        migrations.CreateModel(
+            name='Personalidad',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('foto', models.ImageField(upload_to=b'')),
+                ('nombre', models.CharField(max_length=50)),
+                ('titulo', models.CharField(max_length=250)),
+            ],
+            options={
+                'verbose_name': 'Personalidad',
+                'verbose_name_plural': 'Personalidades',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='entrevistapersonalidad',
+            name='entrevistado',
+            field=models.ForeignKey(to='entrevistas.Personalidad'),
+            preserve_default=True,
         ),
     ]
