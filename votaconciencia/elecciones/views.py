05b689e319837d09c485dd9396c4625bc2ef3328
Marco Lavagnino <marco.lavagnino@gmail.com>
2015-06-18 15:21:20 -0300

    legisladores

--- votaconciencia/elecciones/views.py
+++ votaconciencia/elecciones/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from models import *
 from partidos.models import Partido
 from candidatos.models import Candidato
-from itertools import groupby
+
+def agrupa_por_representacion(**kwargs):
+    grupos_candidatos = {}
+    for candidato in Candidato.objects.filter(**kwargs):
+        try:
+            grupos_candidatos[candidato.representa_a()] += [candidato]
+        except KeyError:
+            grupos_candidatos[candidato.representa_a()] = [candidato]
+    return grupos_candidatos.items()
 
 def index_elecciones(request):
     return render(request, 'eleccion/index.html', {'elecciones': Eleccion.objects.all()})
@@ -17,14 +25,9 @@ def eleccion_informacion(request, id):
     return render(request, 'eleccion/informacion.html', {'eleccion':eleccion})
 
 def eleccion_candidatos(request, id):
-    grupos_candidatos = {}
-    for candidato in Candidato.objects.filter(eleccion__id=id):
-        try:
-            grupos_candidatos[candidato.representa_a()] += [candidato]
-        except KeyError:
-            grupos_candidatos[candidato.representa_a()] = [candidato]
     dictionary = {
-        'candidatos' : grupos_candidatos.items(),
+        'candidatos' : agrupa_por_representacion(eleccion__id=id, legislador=False),
+        'legisladores' : agrupa_por_representacion(eleccion__id=id, legislador=True),
         'eleccion': Eleccion.objects.get(id=id)
     }
     return render(request, 'eleccion/candidatos.html', dictionary)
